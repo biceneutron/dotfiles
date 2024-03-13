84605435bd8178b84f7e3ac90a490d6aa3bbc2f1
biceneutron <biceneutron@gmail.com>
2024-03-13 19:05:21 +0800

    add auto window resize

--- nvim/lua/lc/init.lua
+++ nvim/lua/lc/init.lua
@@ -1,3 +1,15 @@
 require("lc.remap")
 require("lc.set")
 require("lc.packer")
+
+local wr_group = vim.api.nvim_create_augroup('WinResize', { clear = true })
+
+vim.api.nvim_create_autocmd(
+    'VimResized',
+    {
+        group = wr_group,
+        pattern = '*',
+        command = 'wincmd =',
+        desc = 'Automatically resize windows when the host window size changes.'
+    }
+)
